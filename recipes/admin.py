1cb6c5ec5f99c118d64fa7bbf667eeed8b2dc1a9
Alex <alexleeadams@aol.com>
2022-06-06 10:59:22 -0500

    converted list function to list class.

--- recipes/admin.py
+++ recipes/admin.py
@@ -1,4 +1,32 @@
 from django.contrib import admin
 
+from recipes.models import Recipe
+from recipes.models import Step
+from recipes.models import Measure
+from recipes.models import FoodItem
 
 # Register your models here.
+# Create a class that inherits from admin.ModelAdmin
+
+
+class RecipeAdmin(admin.ModelAdmin):
+    pass
+
+
+class MeasureAdmin(admin.ModelAdmin):
+    pass
+
+
+class FoodItemAdmin(admin.ModelAdmin):
+    pass
+
+
+class StepAdmin(admin.ModelAdmin):
+    pass
+
+
+# register the class and model
+admin.site.register(Recipe, RecipeAdmin)
+admin.site.register(Step, StepAdmin)
+admin.site.register(Measure, MeasureAdmin)
+admin.site.register(FoodItem, FoodItemAdmin)
--- recipes/models.py
+++ recipes/models.py
@@ -1,4 +1,62 @@
 from django.db import models
 
-
+# one
 # Create your models here.
+
+
+class Recipe(models.Model):
+    name = models.CharField(max_length=125)
+    author = models.CharField(max_length=100)
+    description = models.TextField()
+    image = models.URLField(null=True)
+    created = models.DateTimeField()
+    content = models.TextField()
+
+    def __str__(self):
+        return f"{self.name}  by {self.author}"
+
+
+# many
+
+
+class Measure(models.Model):
+    name = models.CharField(max_length=30, unique=True)
+    abbreviation = models.CharField(max_length=10, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class FoodItem(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Ingredient(models.Model):
+    amount = models.FloatField()
+    recipe = models.ForeignKey(
+        "Recipe",
+        related_name="ingredients",
+        on_delete=models.CASCADE,
+    )
+    measure = models.ForeignKey("Measure", on_delete=models.PROTECT)
+    food = models.ForeignKey("FoodItem", on_delete=models.PROTECT)
+
+
+class Step(models.Model):
+    order = models.SmallIntegerField()
+    directions = models.TextField()
+
+    recipe = models.ForeignKey(
+        "Recipe",
+        related_name="steps",
+        on_delete=models.CASCADE,
+    )
+    food_items = models.ManyToManyField(
+        "recipes.FoodItem", null=True, blank=True
+    )
+
+    def __str__(self):
+        return self.recipe.name
